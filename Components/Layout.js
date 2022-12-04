d2b349dfd7342c5cce2771a9bc87819688db0419
Dhvanil16 <jaydipadhiyar27@gmail.com>
2022-12-04 23:18:30 +0530

    DarkMode Enabled

--- Components/Layout.js
+++ Components/Layout.js
@@ -1,12 +1,14 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useState } from 'react'
 
 export default function Layout({ children }) {
+  var [darkMode, setDarkMode] = useState(true);
   return (
-    <>
-      <Navbar />
+    <div className={darkMode ? "dark" : ""}>
+      <Navbar darkMode={darkMode} setDarkMode={setDarkMode}/>
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   )
 }
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Link from 'next/link'
 import { BsFillMoonStarsFill } from 'react-icons/bs'
+import { useState } from 'react'
 
-const Navbar = () => {
+const Navbar = ({darkMode,setDarkMode}) => {
+
   return (
     
-    <nav className="py-5 px-10 shadow-sm  flex justify-between border-b-2 bg-opacity-50">
-      <h1 className="text-xl text-neutral-900 font-burtons font-semibold hover:text-orange-500">
+    <nav className="py-5 px-10 shadow-sm  flex justify-between border-b-2 bg-opacity-50 dark:bg-slate-900 ">
+      <h1 className="text-xl text-neutral-900 dark:text-white font-burtons font-semibold hover:text-orange-500">
         <Link href="/">Jaydipsinh Padhiyar</Link>
       </h1>
       <ul className=" flex items-center">
-        {/* <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=" cursor-pointer text-2xl" /></li> */}
+        <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className=" cursor-pointer text-2xl dark:text-slate-50" /></li>
         {/* Without Gradient */}
         {/* <li><a className="bg-cyan-500 text-white px-4 py-2  rounded-md ml-8" href="#">Resume</a></li> */}
         <li>
